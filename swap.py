3f5fbd6da3ba05079d1d3327aa9a71b8dda0fb59
rip-IMRC <somen@hotmail.com>
2025-01-05 11:00:50 -0500

    commit

---
+++ swap.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter number1: "))
+n2=int(input("Enter number2: "))
+print("Value of n1 before swapping", n1)
+print("Value of n2 before swapping", n2)
+temp=n1
+n1=n2
+n2=temp
+print("Value of n1 after swapping", n1)
+print("Value of n2 after swapping", n2)
