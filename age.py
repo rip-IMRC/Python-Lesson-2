59dcc41e5a1a6eb3d5b435dd16e57deaa0b31af8
rip-IMRC <somen@hotmail.com>
2025-01-12 10:50:00 -0500

    commit

---
+++ age.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age: "))
+if age<10:
+    print("You are under age")
+else:
+    if age<=20:
+        print("You are eligible")
+    else:
+        print("You are over aged")
