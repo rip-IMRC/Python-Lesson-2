df7adbecfc9f948889e21bfd08bed4de71440923
rip-IMRC <somen@hotmail.com>
2025-02-09 10:37:18 -0500

    commit

---
+++ floydtriangle.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the number of rows: "))
+number=1
+print("Floyd Triangle")
+for i in range(1, n+1):
+    for j in range (1, i+1):
+        print(number, end=" ")
+        number=number+1
+    print()
