2e696914a7e6c894eddd4a35004d866dc00dfe13
rip-IMRC <somen@hotmail.com>
2025-02-16 10:42:28 -0500

    commit

---
+++ star.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(300, 400)
+polygon=turtle.Turtle()
+for i in range(5):
+    polygon.forward(70)
+    polygon.right(145)
+turtle.done()
