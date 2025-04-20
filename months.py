9baa6111971a0d7073e4e44ce32aceda2bb99894
rip-IMRC <somen@hotmail.com>
2025-04-20 11:27:58 -0400

    commit

---
+++ months.py
@@ -0,0 +1,7 @@
+import calendar
+year = 2025
+month = 5
+print(calendar.month(year, month))
+year = 2025
+print(calendar.calendar(year))
+print(calendar.month_name[1:])
--- numbergame.py
+++ numbergame.py
@@ -1,10 +1,12 @@
 import random
 playing=True
-number=str(random.randint(10, 20))
+number=int(random.randint(10, 20))
 print("I will generate a number from 10 to 20, you have to guess one number at a time")
 print("The game ends when you get it")
 while playing:
-    guess=input("Give it your best guess! \n")
+    guess=int(input("Give it your best guess! \n"))
+    if guess>20 or guess<10:
+        print("Type a number between 10 and 20")
     if number==guess:
         print("You win")
         print("The number was", number)
