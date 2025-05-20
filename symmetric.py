d080d968e3476ca76e2ffeedc30eb1c420bf7188
rip-IMRC <somen@hotmail.com>
2025-05-19 20:16:37 -0400

    commit

---
+++ symmetric.py
@@ -0,0 +1,6 @@
+def symmetric_difference(set1, set2):
+  return set1.symmetric_difference(set2)
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+symmetric_difference_result = symmetric_difference(set1, set2)
+print(symmetric_difference_result) 
