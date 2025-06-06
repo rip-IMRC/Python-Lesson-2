b6a1731c2727e39e8a4a4d1d1f1ffa5942d471ae
rip-IMRC <somen@hotmail.com>
2025-06-06 14:03:16 -0400

    commit

--- dogabove.py
+++ dogabove.py
@@ -3,8 +3,8 @@ class Dog:
     def __init__(self, breed, color):
         self.breed=breed
         self.color=color
-Miku=Dog("Golden Retriever", "Gold")
-Chiku=Dog("Labrador", "Black")
+Miku=Dog(input("Choose a dog breed: "), input("Choose a color: "))
+Chiku=Dog(input("Choose a dog breed: "), input("Choose a color: "))
 print("Miku is a {}".format(Miku.species))
 print("Chiku is also a {}".format(Chiku.species))
 print("Miku is a {} and is the color {}".format(Miku.breed, Miku.color))
